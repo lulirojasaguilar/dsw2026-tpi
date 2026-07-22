4c3d511731702b1f6b2b8f9773dace0030f9a231
LUZ <lulirojasaguilar@gmail.com>
2026-07-22 17:15:00 -0300

    Complete la autenticación de pacientes (RN06) y la colección Postman

---
+++ Dsw2026Tpi.Data/Configurations/PatientConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Tpi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Dsw2026Tpi.Data.Configurations
+{
+    public class PatientConfiguration : IEntityTypeConfiguration<Patient>
+    {
+        public void Configure(EntityTypeBuilder<Patient> builder)
+        {
+            builder.ToTable("Patients");
+
+            builder.Property(p => p.Dni)
+                .IsRequired();
+
+            builder.HasIndex(p => p.Dni)
+                .IsUnique();
+
+            builder.Property(p => p.Email)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            builder.Property(p => p.ApplicationUserId)
+                .IsRequired()
+                .HasMaxLength(450);
+
+            builder.Property(p => p.Deleted)
+                .HasColumnName("deleted")
+                .HasDefaultValue(false);
+        }
+    }
+}
