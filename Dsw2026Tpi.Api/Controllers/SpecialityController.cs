8f0f922d84eb718dbbbcb4376a27f9b224c0d1ba
soffiacamposs <SofiaCampos@alu.frt.utn.edu.ar>
2026-07-22 23:57:13 -0300

    [specialties] Implemente CRUD de Speciality

---
+++ Dsw2026Tpi.Api/Controllers/SpecialityController.cs
@@ -0,0 +1,55 @@
+﻿using Dsw2026Tpi.Application.Dtos;
+using Dsw2026Tpi.Application.Interfaces;
+using Dsw2026Tpi.CrossCutting.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dsw2026Tpi.Api.Controllers;
+
+[Route("api/specialties")]
+[Authorize(Policy = Policies.AdminPolicy)]
+   public class SpecialityController : AppController
+   {
+       private readonly ISpecialityService _service;
+       public SpecialityController(ISpecialityService service)
+       {
+           _service = service;
+       }
+
+       [HttpGet]
+       [ProducesResponseType(StatusCodes.Status200OK)]
+       public async Task<IActionResult> GetAll([FromQuery] int pageSize, [FromQuery] int pageIndex, [FromQuery] string? name = null)
+       {
+           var specialities = await _service.GetAll(pageSize, pageIndex, name);
+           return Ok(specialities);
+       }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> Create([FromBody] SpecialityModel.Request request)
+    {
+        var speciality = await _service.Create(request);
+
+        return StatusCode(StatusCodes.Status201Created, speciality);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> Update(
+    [FromRoute] Guid id,
+    [FromBody] SpecialityModel.Request request)
+    {
+        var speciality = await _service.Update(id, request);
+
+        return Ok(speciality);
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> Delete([FromRoute] Guid id)
+    {
+        await _service.Delete(id);
+
+        return NoContent();
+    }
+}
--- Dsw2026Tpi.Data/Configurations/SpecialityConfiguration.cs
+++ Dsw2026Tpi.Data/Configurations/SpecialityConfiguration.cs
@@ -9,5 +9,16 @@ public class SpecialityConfiguration : IEntityTypeConfiguration<Speciality>
     public void Configure(EntityTypeBuilder<Speciality> builder)
     {
         builder.ToTable("Specialities");
+
+        builder.Property(s => s.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.HasIndex(s => s.Name)
+            .IsUnique();
+
+        builder.Property(s => s.Description)
+            .IsRequired()
+            .HasMaxLength(100);
     }
 }
