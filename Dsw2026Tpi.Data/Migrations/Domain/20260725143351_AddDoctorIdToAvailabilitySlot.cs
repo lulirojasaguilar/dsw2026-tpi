13b0f939e3c1a14e39ab1ae3aeea3a17a3cab3b5
Juan Pablo <chulolules@gmail.com>
2026-07-27 14:56:40 -0300

    fix(migration) correccion palabra reservada rule en la migracion de SQL

--- Dsw2026Tpi.Data/Migrations/Domain/20260725143351_AddDoctorIdToAvailabilitySlot.cs
+++ Dsw2026Tpi.Data/Migrations/Domain/20260725143351_AddDoctorIdToAvailabilitySlot.cs
@@ -21,13 +21,18 @@ namespace Dsw2026Tpi.Data.Migrations.Domain
                 type: "uniqueidentifier",
                 nullable: true);
 
-            migrationBuilder.Sql("""
-                UPDATE slot
-                SET slot.DoctorId = rule.DoctorId
-                FROM AvailabilitySlot AS slot
-                INNER JOIN AvailabilityRule AS rule
-                    ON slot.AvailabilityRuleId = rule.Id;
-                """);
+            migrationBuilder.Sql(@"
+   UPDATE slot
+
+    SET slot.DoctorId = availabilityRule.DoctorId
+
+    FROM AvailabilitySlot AS slot
+
+    INNER JOIN AvailabilityRule AS availabilityRule
+
+        ON slot.AvailabilityRuleId = availabilityRule.Id;
+
+    """);
 
             migrationBuilder.AlterColumn<Guid>(
                 name: "DoctorId",
