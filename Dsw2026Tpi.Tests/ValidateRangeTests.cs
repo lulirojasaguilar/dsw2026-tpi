f99019015f7099a9f35538937de6046fe061c4dd
Ornella Risso Verges <ornellarissoverges@alu.frt.utn.edu.ar>
2026-08-02 23:51:43 -0300

    Primer test de ValidateRange

---
+++ Dsw2026Tpi.Tests/ValidateRangeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Dsw2026Tpi.Application.Services;
+using Xunit;
+
+namespace Dsw2026Tpi.Tests;
+
+public class ValidateRangeTests
+{
+    [Fact]
+    public void ValidateRange_CuandoElRangoEsValido_NoDeberiaLanzarExcepcion()
+    {
+        // Arrange
+        var startTime = new TimeSpan(8, 0, 0);
+        var endTime = new TimeSpan(9, 0, 0);
+
+        // Act
+        var exception = Record.Exception(() =>
+            AvailabilityGenerator.ValidateRange(startTime, endTime));
+
+        // Assert
+        Assert.Null(exception);
+    }
+}
